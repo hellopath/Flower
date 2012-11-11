313bbd061ae4205bf80c9492688fba309b198e7b
hellopath <thomogloupanagiotis@gmail.com>
2012-11-11 22:52:41 +0100

    make a first example of the flower and leaves position. Also there is a boilareplate index.html so to start directly your examples

---
+++ asset/3Dobject/leaf.js
@@ -0,0 +1,23 @@
+{
+
+    "metadata" :
+    {
+        "formatVersion" : 3.1,
+        "sourceFile"    : "leaf.obj",
+        "generatedBy"   : "OBJConverter",
+        "vertices"      : 9,
+        "faces"         : 7,
+        "normals"       : 0,
+        "uvs"           : 9,
+        "materials"     : 0
+    },
+
+    "materials": [	{
+	"DbgColor" : 15658734,
+	"DbgIndex" : 0,
+	"DbgName" : "default"
+	}],
+
+    "buffers": "leaf.bin"
+
+}
